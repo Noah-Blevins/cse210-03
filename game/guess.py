39758f73881a4894bec957084fcb8aedc962202b
Noah-Blevins <noah.james.blevins@gmail.com>
2022-02-04 17:50:39 -0700

    Updated specifications

--- game/guess.py
+++ game/guess.py
@@ -1,6 +1,6 @@
 # Specifications:
 
 # •	Guess class
-#     o	Gets a word from Words()
+#     o	Gets a user-defined word upon initialization
 #     o	Generates blanks, as well as the correct letters guessed
 #     o	A method to determine if the word is guessed
